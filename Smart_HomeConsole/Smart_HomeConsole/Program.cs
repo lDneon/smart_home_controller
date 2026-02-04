bb6820d0f6959c87b8140f7ed1f4139990b89b89
lDneon <debreuslovemica@gmail.com>
2026-02-03 19:42:11 -0500

    home hub created

---
+++ Smart_HomeConsole/Smart_HomeConsole/Program.cs
@@ -0,0 +1,15 @@
+﻿
+using Smart_HomeLib;
+
+var hub = new Smart_homeHub();
+
+// You will add your devices here once you implement them.
+// Example flow you should be able to run by the end:
+// - create devices
+// - SetOnline(true)
+// - TurnOn()
+// - ApplyModeToAll("Night")
+// - PrintAllStatuses()
+
+Console.WriteLine("SmartHomeConsole starting...");
+Console.WriteLine("Add device creation and hub actions once classes are implemented.");
